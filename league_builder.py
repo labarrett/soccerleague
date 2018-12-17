e66731571f8fc79738d3aef87e7005e1b042fb26
Lindsay Barrett <labarrett22@gmail.com>
2018-12-16 18:33:27 -0800

    Comment instructions

---
+++ league_builder.py
@@ -0,0 +1,20 @@
+import csv
+
+
+#open file
+with open('soccer_players.csv', newline='') as csvfile:
+    playerreader=csv.reader(csvfile,delimeter='|')
+    rows=list(playerreader)
+    for row in rows
+
+#read file
+
+#pull out file information
+
+#loop through info and add to appropariate lists
+
+#take new lists and input them to a new file in order of TD instructions 
+with open("teams.txt", "a") as file:
+    file.write(LIST NAMES)
+
+
