782d0d83eadae63942b8f9d20f65ae6c73465689
Lindsay Barrett <labarrett22@gmail.com>
2018-12-16 19:43:13 -0800

    Pulling out file data

--- league_builder.py
+++ league_builder.py
@@ -1,20 +1,23 @@
+
 import csv
 
 
-#open file
-with open('soccer_players.csv', newline='') as csvfile:
-    playerreader=csv.reader(csvfile,delimeter='|')
+#open and file
+with open('soccer_players.csv') as csvfile:
+    playerreader=csv.DictReader(csvfile)
     rows=list(playerreader)
-    for row in rows
-
-#read file
-
+    for row in rows:
+        print(row['Name'],row['Soccer Experience'])
+       
+       
+        
+       
 #pull out file information
 
 #loop through info and add to appropariate lists
 
 #take new lists and input them to a new file in order of TD instructions 
-with open("teams.txt", "a") as file:
-    file.write(LIST NAMES)
+#with open("teams.txt", "a") as file:
+ #   file.write(LIST NAMES)
 
 
