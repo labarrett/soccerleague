3e7bf3330180cec53e78b96555fd51b070b38b7b
Lindsay Barrett <labarrett22@gmail.com>
2018-12-17 21:20:43 -0800

    Added write to file

--- league_builder.py
+++ league_builder.py
@@ -3,10 +3,8 @@ import csv
 
 #teams to assign
 yes_players=[]
+no_players=[]
 
-shark=[]
-dragons=[]
-raptors=[]
 
 #open and file
 with open('soccer_players.csv') as csvfile:
@@ -15,17 +13,40 @@ with open('soccer_players.csv') as csvfile:
     player=[]
     for row in rows:        
         if row['Soccer Experience']== "YES":
-            yes_players.append(row['Name'])
+            yes_players.append(tuple([row['Name'],row['Soccer Experience'],row["Guardian Name(s)"]]))
+            
+        else:
+            no_players.append(tuple([row['Name'],row['Soccer Experience'],row["Guardian Name(s)"]]))
+        
 #shark.append(player)
-print(yes_players)
+
+
+size_needed= len(yes_players)/3
+no_size_needed = len(no_players)/3
+
+shark=yes_players[:size_needed] + no_players[:no_size_needed]
+dragons=yes_players[size_needed: (size_needed * 2)] + no_players[no_size_needed: (no_size_needed * 2)]
+raptors=yes_players[(size_needed * 2):] + no_players[(no_size_needed * 2):]
+
+def commas():
+ print("*" * 25)
+
+commas()
+print("SHARKS:", shark)
+commas()
+print("DRAGONS:", dragons)
+commas()
+print("RAPTORS:", raptors)
+commas()
        
-#pull out file information
 
-#loop through info and add to appropariate lists
 
-#take new lists and input them to a new file in order of TD instructions 
-#with open("teams.txt", "a") as file:
- #   file.write(LIST NAMES)
+#right to file
+with open("teams.txt", "a") as file:
+    file.write(str(shark))
+    file.write(str(dragons))
+    file.write(str(raptors))
+    file.close()
 
  #row['Soccer Experience'],
               #row["Guardian Name(s)"],
