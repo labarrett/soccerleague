ecf5d2975164c3a67958fd80218203a0c9babe1a
Lindsay Barrett <labarrett22@gmail.com>
2018-12-19 14:42:32 -0700

    Adding new file options

--- league_builder.py
+++ league_builder.py
@@ -1,4 +1,3 @@
-
 import csv
 
 #teams to assign
@@ -21,9 +20,10 @@ with open('soccer_players.csv') as csvfile:
 #shark.append(player)
 
 
-size_needed= len(yes_players)/3
-no_size_needed = len(no_players)/3
+size_needed= int(len(yes_players)/3)
+no_size_needed = int(len(no_players)/3)
 
+print(yes_players)
 shark=yes_players[:size_needed] + no_players[:no_size_needed]
 dragons=yes_players[size_needed: (size_needed * 2)] + no_players[no_size_needed: (no_size_needed * 2)]
 raptors=yes_players[(size_needed * 2):] + no_players[(no_size_needed * 2):]
@@ -38,16 +38,29 @@ print("DRAGONS:", dragons)
 commas()
 print("RAPTORS:", raptors)
 commas()
-       
+
+
+teams = {
+    'sharks': shark,
+    'raptors': dragons,
+    'dragons': raptors
+}       
 
 
 #right to file
 with open("teams.txt", "a") as file:
-    file.write(str(shark))
-    file.write(str(dragons))
-    file.write(str(raptors))
+    file.write("SHARKS *********\n")
+    file.write(str(teams.get('sharks')))
+    file.write("\nRAPTORS *********\n")
+    file.write(str(teams.get('raptors')))
+    file.write("\nDRAGONS *********\n")
+    file.write(str(teams.get('dragons')))
+   
     file.close()
 
  #row['Soccer Experience'],
               #row["Guardian Name(s)"],
               #row["Height (inches)"]
+
+ #    file.write(str(dragons))
+#    file.write(str(raptors))
